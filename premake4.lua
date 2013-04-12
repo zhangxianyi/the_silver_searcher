dfbb75249554f55522ea89955a2a31b14600f052
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2013-04-12 16:24:51 -0700

    started on windows port

---
+++ premake4.lua
@@ -0,0 +1,55 @@
+-- to generate Visual Studio files in vs-premake directory, run:
+-- premake4 vs2010 or premake4 vs2008
+
+-- common settings for solutions
+function solution_common()
+  configurations { "Debug", "Release" }
+  location "vs-premake" -- this is where generated solution/project files go
+
+  -- Symbols - generate .pdb files
+  -- StaticRuntime - statically link crt
+  -- ExtraWarnings - set max compiler warnings level
+  -- FatalWarnings - compiler warnigs are errors'
+  -- NoMinimalRebuild - disable /Gm because it clashes with /MP
+  flags {
+   "Symbols", "StaticRuntime", "ExtraWarnings",
+   "NoRTTI", "Unicode", "NoExceptions", "NoMinimalRebuild"
+  }
+
+  configuration "Debug"
+    targetdir "dbg" -- this is where the .exe/.lib etc. files wil end up
+    defines { "_DEBUG", "DEBUG" }
+
+  configuration "Release"
+     targetdir "rel"
+     flags { "Optimize" }
+     defines { "NDEBUG" }
+     -- 4189 - variable not used, happens with CrashIf() macros that are no-op
+     --        in release builds
+     buildoptions { "/wd4189" }
+
+  configuration {"vs*"}
+    -- defines { "_WIN32", "WIN32", "WINDOWS", "_CRT_SECURE_NO_WARNINGS" }
+    defines { "_WIN32", "WIN32", "WINDOWS" }
+    -- 4800 - int -> bool coversion
+    -- 4127 - conditional expression is constant
+    -- 4100 - unreferenced formal parameter
+    -- 4244 - possible loss of data due to conversion
+    -- /MP  - use multi-cores for compilation
+    buildoptions {
+        "/wd4800", "/wd4127", "/wd4100", "/wd4244"
+    }
+end
+
+solution "ag"
+  solution_common()
+
+  project "ag"
+    kind "ConsoleApp"
+    language "C++"
+    files {
+      "src/*.h",
+      "src/*.c",
+    }
+    includedirs { "src", "wincompat", "wincompat/zlib" }
+    links { "Shlwapi" }
--- src/options.c
+++ src/options.c
@@ -3,9 +3,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#ifndef _MSC_VER
 #include <sys/param.h>
+#endif
 #include <sys/stat.h>
+#ifndef _MSC_VER
 #include <unistd.h>
+#endif
 
 #include "config.h"
 #include "ignore.h"
