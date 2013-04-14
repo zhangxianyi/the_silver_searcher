9ea9064ea2c352cb278ad2017b8b604f09bd5a4f
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2013-04-13 20:50:12 -0700

    add getopt implementation

--- wincompat/getopt.h
+++ wincompat/getopt.h
@@ -1,32 +1,60 @@
-#ifndef wincompat_getopt_h
-#define wincompat_getopt_h
+#ifndef INCLUDED_GETOPT_PORT_H
+#define INCLUDED_GETOPT_PORT_H
 
-#ifdef __cplusplus
-extern "C"
-{
+/* from https://github.com/kimgr/getopt_port
+
+Copyright (c) 2012, Kim Gräsman
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of Kim Gräsman nor the
+      names of contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL KIM GRÄSMAN BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+
+#if defined(__cplusplus)
+extern "C" {
 #endif
 
-#define null_argument       0
-#define no_argument         0
-#define required_argument   1
-#define optional_argument   2
+extern const int no_argument;
+extern const int required_argument;
+extern const int optional_argument;
+
+extern char* optarg;
+extern int optind, opterr, optopt;
 
 struct option {
-    const char* name;
-    int         has_arg;
-    int *       flag;
-    char        val;
+  const char* name;
+  int has_arg;
+  int* flag;
+  int val;
 };
 
-int getopt_long(int argc, char * const argv[], const char *optstring,
-           const struct option *longopts, int *longindex);
+int getopt(int argc, char* const argv[], const char* optstring);
 
-int getopt_long_only(int argc, char * const argv[], const char *optstring,
-                  const struct option *longopts, int *longindex);
+int getopt_long(int argc, char* const argv[],
+  const char* optstring, const struct option* longopts, int* longindex);
 
-
-#ifdef __cplusplus
+#if defined(__cplusplus)
 }
 #endif
 
-#endif
+#endif // INCLUDED_GETOPT_PORT_H
--- wincompat/unistd.h
+++ wincompat/unistd.h
@@ -1,18 +1,14 @@
 #ifndef wincompat_unistd_h
 #define wincompat_unistd_h
 
+#include "getopt.h"
+
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
 int getpagesize();
-
-/* implemented in getopt.c */
-int getopt(int argc, char * const argv[], const char *optstring);
-
-extern char *optarg;
-extern int optind, opterr, optopt;
 
 #ifdef __cplusplus
 }
