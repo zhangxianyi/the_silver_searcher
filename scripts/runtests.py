631062d5c234a30f0929d93ed334c88566e6b822
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2013-10-29 13:59:35 -0700

    add -clean option to runtests.bat/runtests.py

--- scripts/runtests.py
+++ scripts/runtests.py
@@ -23,12 +23,15 @@ To make it easy and compact to write additional tests:
 import sys, os, shutil, codecs
 import util
 
+
 # set to True only when testing, speeds up test cycle by not compiling
 # the executable if it already exists
 NO_BUILD_IF_EXE_EXISTS = True
 
+
 def fatal(msg):
 	print(msg); sys.exit(1)
+
 
 @util.memoize
 def top_level_dir():
@@ -41,29 +44,39 @@ def top_level_dir():
 	os.chdir(path)
 	return path
 
+
 @util.memoize
 def ag_tests_dir():
 	return os.path.realpath(os.path.join(top_level_dir(), "..", "ag_tests"))
 
-def delete_ag_tests_dir():
-	path = ag_tests_dir()
+
+def delete_dir(path):
 	if os.path.exists(path):
 		shutil.rmtree(path, True)
+
+
+def delete_ag_tests_dir():
+	delete_dir(ag_tests_dir())
+
 
 def recreate_ag_tests_dir():
 	delete_ag_tests_dir()
 	path = ag_tests_dir()
 	os.mkdir(path)
 
+
 def is_win():
 	return sys.platform.startswith("win")
+
 
 def is_mac():
 	return sys.platform == "darwin"
 
+
 @util.memoize
 def ag_exe_path_win():
 	return os.path.join(top_level_dir(), "rel", "ag.exe")
+
 
 @util.memoize
 def ag_exe_path():
@@ -73,10 +86,12 @@ def ag_exe_path():
 		# mac and unix
 		return os.path.realpath(os.path.join(top_level_dir(), "ag"))
 
+
 def verify_started_in_right_directory():
 	path = os.path.join(top_level_dir(), "scripts", "runtests.py")
 	if not os.path.exists(path):
 		fatal("Must be in top level of source directory and run as ./scripts/runtests.py.\npath=%s" % path)
+
 
 # just to be safe, if ../ag_tests directory exists we won't run tests and
 # ask the user to delete it manually. This is either a conflict with a directory
@@ -86,10 +101,12 @@ def verify_ag_tests_dir_doesnt_exist():
 	print("ag_tests directory ('%s') directory from previous run exist" % ag_tests_dir())
 	fatal("Please delete it manually.")
 
+
 def print_error(out, err, errcode):
 	print("Failed with error code %d" % errcode)
 	if len(out) > 0: print("Stdout:\n%s" % out)
 	if len(err) > 0: print("Stderr:\n%s" % err)
+
 
 def build_mac():
 	(out, err, errcode) = util.run_cmd("./build.sh")
@@ -101,12 +118,16 @@ def build_mac():
 			fatal("\nIf you're using homebrew, you need to install xz package to get liblzma\nRun: brew install xz")
 		sys.exit(1)
 
+
 def build_win():
 	util.run_cmd_throw("premake4", "vs2010")
+	curr_dir = os.getcwd()
 	os.chdir("vs-premake")
 	util.kill_msbuild()
 	util.run_cmd_throw("devenv", "ag.sln", "/Build", "Release", "/Project", "ag.vcxproj")
 	assert os.path.exists(ag_exe_path_win()), "%s doesn't exist" % ag_exe_path_win()
+	os.chdir(curr_dir)
+
 
 # TODO: support unix?
 def build():
@@ -119,20 +140,24 @@ def build():
 	else:
 		fatal("Don't know how to build on this platform. sys.platform=%s, os.name=%s" % (sys.platform, os.name))
 
+
 def path_unix_to_native(path):
 	parts = path.split("/")
 	return os.path.join(*parts)
+
 
 def create_dir_for_file(path):
 	dir = os.path.dirname(path)
 	if not os.path.exists(dir):
 		os.makedirs(dir)
 
+
 # here path is always in unix format
 def write_to_file(path, content):
 	create_dir_for_file(path)
 	with codecs.open(path, "wb", "utf8") as fo:
 		fo.write(content)
+
 
 class FileInfo(object):
 	def __init__(self, path):
@@ -144,10 +169,12 @@ class FileInfo(object):
 		p = os.path.join(dir, path_unix_to_native(self.path))
 		write_to_file(p, self.data)
 
+
 class CmdInfo(object):
 	def __init__(self, cmd, expected):
 		self.cmd = cmd
 		self.expected = expected
+
 
 class TestInfo(object):
 	def __init__(self):
@@ -155,6 +182,7 @@ class TestInfo(object):
 		# TODO: should test_name be part of CmdInfo?
 		self.test_name = "" # optional
 		self.cmds = [] # of CmdInfo
+
 
 # context for parsing functions
 class Tests(object):
@@ -194,10 +222,12 @@ class Tests(object):
 		print("Curr line no %d:\n'%s'" % (self.curr_line, curr_line))
 		raise "Error"
 
+
 TEST_START = "test:"
 CMD_START = "cmd:"
 FILE_START = "file:"
 NEW_TEST = "--"
+
 
 # helper function that chooses next parsing function based on current line
 def select_parsing_function(tests, *valid):
@@ -215,6 +245,7 @@ def select_parsing_function(tests, *valid):
 	# TODO: more cases?
 	raise BaseException("Invalid state, curr_line=`%s`, valid=%s" % (curr_line, str(valid)))
 
+
 def read_file_cmd_content(tests):
 	data_lines = []
 	while True:
@@ -226,6 +257,7 @@ def read_file_cmd_content(tests):
 				tests.unget_line()
 				return "\n".join(data_lines)
 		data_lines.append(line)
+
 
 # file: ...
 # ... # file data
@@ -239,6 +271,7 @@ def parse_file_line(tests):
 	tests.curr_test_info.files.append(fileInfo)
 	#print("File data: \n'%s'\n" % fileInfo.data)
 	return select_parsing_function(tests, CMD_START, FILE_START)
+
 
 # cmd: ...
 # ... # output of the command
@@ -260,6 +293,7 @@ def parse_cmd_line(tests):
 	tests.unget_line()
 	return parse_cmd_line
 
+
 # test: ...
 def parse_test_line(tests):
 	assert tests.curr_test_info.test_name == "" # shouldn't be callsed twice
@@ -268,9 +302,11 @@ def parse_test_line(tests):
 	tests.curr_test_info.test_name = parts[1].strip()
 	return select_parsing_function(tests, NEW_TEST, CMD_START, FILE_START)
 
+
 def parse_new_test(tests):
 	tests.start_new_test()
 	return select_parsing_function(tests, TEST_START, CMD_START, FILE_START)
+
 
 def parse_at_file_start(tests):
 	while True:
@@ -284,6 +320,7 @@ def parse_at_file_start(tests):
 			return parse_new_test(tests)
 		tests.raise_error()
 
+
 # returns an array of TestInfo objects
 def parse_test_file(test_file):
 	tests = Tests(test_file)
@@ -291,6 +328,7 @@ def parse_test_file(test_file):
 	while parse_func != None:
 		parse_func = parse_func(tests)
 	return tests
+
 
 def run_ag_and_verify_results(cmd_info):
 	args = [ag_exe_path()] + cmd_info.cmd.split()
@@ -309,6 +347,7 @@ def run_ag_and_verify_results(cmd_info):
 	if result != expected:
 		fatal("Unexpected value. Stdout:\n'%s'\nExpected:\n'%s'\n" % (result, expected))
 
+
 def run_one_test(test_info, test_no):
 	recreate_ag_tests_dir()
 	for file_info in test_info.files:
@@ -321,6 +360,7 @@ def run_one_test(test_info, test_no):
 	map(run_ag_and_verify_results, test_info.cmds)
 	os.chdir(dir)
 
+
 def run_tests_in_file(test_file):
 	print(test_file)
 	tests = parse_test_file(test_file)
@@ -329,6 +369,7 @@ def run_tests_in_file(test_file):
 	for test_info in tests.test_infos:
 		run_one_test(test_info, test_no)
 		test_no += 1
+
 
 def run_tests():
 	ag_cmd = ag_exe_path()
@@ -339,17 +380,43 @@ def run_tests():
 	# if everything went ok, delete the temporary tests directory
 	delete_ag_tests_dir()
 
+
 def verify_ag_exe_exists():
 	(out, err) = util.run_cmd_throw(ag_exe_path(), "--version")
 	print(out)
 
+
+def clean_win():
+	delete_ag_tests_dir()
+	delete_dir(os.path.join(top_level_dir(), "rel"))
+	delete_dir(os.path.join(top_level_dir(), "vs-premake"))
+
+
+def clean_mac():
+	delete_ag_tests_dir()
+	assert False, "TODO: finish implementing clean_mac()"
+
+
+def clean():
+	print("Doing a clean rebuild")
+	if is_win():
+		clean_win()
+	elif is_mac():
+		clean_mac()
+	else:
+		assert False, "Unknown platform (not win or mac)"
+
+
 def main():
 	#print("top_level_dir = %s\nag_tests_dir =  %s\n" % (top_level_dir(), ag_tests_dir()))
  	verify_started_in_right_directory()
+	if "-clean" in sys.argv:
+		clean()
 	verify_ag_tests_dir_doesnt_exist()
 	build()
 	verify_ag_exe_exists()
 	run_tests()
+
 
 if __name__ == "__main__":
 	main()
